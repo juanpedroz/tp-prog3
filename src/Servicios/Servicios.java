e7fc0242a4c1dc1faf64a8c6bee36e3115b7c2c7
Juan Pedro Zugbi <jzugbi@gmail.com>
2026-06-14 20:09:42 -0300

    servicio 3

--- src/Servicios/Servicios.java
+++ src/Servicios/Servicios.java
@@ -4,10 +4,7 @@ import Clases.Camion;
 import Clases.Paquete;
 import Reader.CSVReader;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Servicios {
     //Completar con las estructuras y métodos privados que se requieran.
@@ -16,6 +13,7 @@ public class Servicios {
     private Map<String, Paquete> paquetesXcodigo;
     private List<Paquete> paquetesConAlimentos;
     private List<Paquete> paquetesSinAlimentos;
+    private Hashtable<Integer, List<Paquete>> paquetesXUrgencia;
 
     /*
      * Expresar la complejidad temporal del constructor.
@@ -25,12 +23,27 @@ public class Servicios {
         this.camiones = CSVReader.cargarCamiones(pathCamiones);
         this.paquetes = CSVReader.cargarPaquetes(pathPaquetes);
 
+        /*
+        * no me queda claro si está bien que el constructor tenga todo esto,
+        * pero no se estarían generando todas las estructuras al instanciar el servicio.
+        */
         for (Paquete p : paquetes) {
-            this.paquetesXcodigo.put(p.getCodIdentificador(),p);
-            if(p.getContieneAlimentos()){
+            this.paquetesXcodigo.put(p.getCodIdentificador(),p); //SERVICIO 1
+
+            if(p.getContieneAlimentos()){ //SERVICIO 2
                 this.paquetesConAlimentos.add(p);
             } else {
                 this.paquetesSinAlimentos.add(p);
+            }
+
+            if (this.paquetesXUrgencia.containsKey(p.getUrgencia())) { //SERVICIO 3
+                List<Paquete> listaPaquetes = paquetesXUrgencia.get(p.getUrgencia());
+                listaPaquetes.add(p);
+                this.paquetesXUrgencia.put(p.getUrgencia(),listaPaquetes);
+            } else {
+                List<Paquete> listaPaquetes = new ArrayList<>();
+                listaPaquetes.add(p);
+                this.paquetesXUrgencia.put(p.getUrgencia(),listaPaquetes);
             }
         }
 
@@ -43,14 +56,14 @@ public class Servicios {
     //    Servicio 1: Dado un código de paquete (String), retornar toda la información del paquete asociado.
 //    En caso de no existir, retornar null.
     public Paquete servicio1(String codigoPaquete) { // O(1)
-        return paquetesXcodigo.get(codigoPaquete);// tendria que controlar el null??? entiendo que el map lo hace
+        return paquetesXcodigo.get(codigoPaquete);
     }
     /*
      * Expresar la complejidad temporal del servicio 2.
      */
 //    Servicio 2: Dado un booleano que indica si se buscan paquetes que contienen alimentos (true)
 //    o que no contienen alimentos (false), retornar el listado de paquetes correspondiente.
-    public List<Paquete> servicio2(boolean contieneAlimentos) {
+    public List<Paquete> servicio2(boolean contieneAlimentos) { // O(1)
         if(contieneAlimentos){
             return paquetesConAlimentos;
         }
@@ -61,7 +74,13 @@ public class Servicios {
      */
 //    Servicio 3: Dados dos valores enteros que representan un nivel de urgencia mínimo y máximo,
 //    retornar todos los paquetes cuyo nivel de urgencia se encuentre dentro de ese rango (inclusive).
-    public List<Paquete> servicio3(int urgenciaMinima, int urgenciaMaxima) {
-        return null;
+    public List<Paquete> servicio3(int urgenciaMinima, int urgenciaMaxima) { // O(n) donde n es la diferencia entre urgenciaMaxima y urgenciaMinima
+        List<Paquete> listaPaquetes = new ArrayList<>();
+        for (int i = urgenciaMinima; i <= urgenciaMaxima; i++) {
+            if(paquetesXUrgencia.containsKey(i)){
+                listaPaquetes.addAll(paquetesXUrgencia.get(i));
+            }
+        }
+        return listaPaquetes;
     }
 }
