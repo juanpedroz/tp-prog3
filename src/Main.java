54a68cff5905c70049f84b10452bc4c745f8516f
Manuel Dualde <manueldualde2012@gmail.com>
2026-06-17 17:08:49 -0300

    se actualiza el main

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,30 @@
-import Clases.Camion;
-import Clases.Paquete;
-import Reader.CSVReader;
+import Algoritmos.Backtracking;
+import Algoritmos.Greedy;
+import Clases.Solucion;
 import Servicios.Servicios;
-
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-//        List<Camion> camiones = CSVReader.cargarCamiones("src/Datos/Camiones.csv");
-//        List<Paquete> paquetes = CSVReader.cargarPaquetes("src/Datos/Paquetes.csv");
 
-        Servicios servicios = new Servicios("src/Datos/Camiones.csv","src/Datos/Paquetes.csv");
-
-//        System.out.println("\n=== CAMIONES ===");
-//        for (Camion c : camiones) {
-//            System.out.println(c);
-//        }
-//
-//        System.out.println("\n=== PAQUETES ===");
-//        for (Paquete p : paquetes) {
-//            System.out.println(p);
-//        }
+        Servicios servicios = new Servicios("src/Datos/Camiones.csv", "src/Datos/Paquetes.csv");
 
         System.out.println("\n=== SERVICIOS ===");
         System.out.println(servicios.servicio1("P005"));
-        System.out.println(servicios.servicio1("P999")); // debería dar null
-
+        System.out.println(servicios.servicio1("P999"));
         System.out.println(servicios.servicio2(true));
         System.out.println(servicios.servicio2(false));
-
         System.out.println(servicios.servicio3(1, 10));
         System.out.println(servicios.servicio3(50, 100));
 
+        System.out.println("\n=== BACKTRACKING ===");
+        Backtracking bt = new Backtracking();
+        Solucion solucionBT = bt.resolver(servicios.getCamiones(), servicios.getPaquetes());
+        System.out.println(solucionBT);
+
+        System.out.println("\n=== GREEDY ===");
+        Greedy greedy = new Greedy();
+        Solucion solucionGreedy = greedy.resolver(servicios.getCamiones(), servicios.getPaquetes());
+        System.out.println(solucionGreedy);
     }
 }
