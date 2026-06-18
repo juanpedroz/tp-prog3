7438a5ac7a24773fd34703a80f8dcd198689d3ab
Juan Pedro Zugbi <jzugbi@gmail.com>
2026-06-18 10:47:29 -0300

    Modificaciones bobas

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,28 @@ public class Main {
         Servicios servicios = new Servicios("src/Datos/Camiones.csv", "src/Datos/Paquetes.csv");
 
         System.out.println("\n=== SERVICIOS ===");
+
         System.out.println("\n=== SERVICIO 1 ===");
         System.out.println(servicios.servicio1("P005"));
         System.out.println(servicios.servicio1("P999"));
+
         System.out.println("\n=== SERVICIO 2 ===");
         System.out.println(servicios.servicio2(true));
         System.out.println(servicios.servicio2(false));
+
         System.out.println("\n=== SERVICIO 3 ===");
         System.out.println(servicios.servicio3(1, 10));
         System.out.println(servicios.servicio3(50, 100));
+
+        System.out.println("\n=== GREEDY ===");
+        Greedy greedy = new Greedy();
+        Solucion solucionGreedy = greedy.resolver(servicios.getCamiones(), servicios.getPaquetes());
+        System.out.println(solucionGreedy);
 
         System.out.println("\n=== BACKTRACKING ===");
         Backtracking bt = new Backtracking();
         Solucion solucionBT = bt.resolver(servicios.getCamiones(), servicios.getPaquetes());
         System.out.println(solucionBT);
 
-        System.out.println("\n=== GREEDY ===");
-        Greedy greedy = new Greedy();
-        Solucion solucionGreedy = greedy.resolver(servicios.getCamiones(), servicios.getPaquetes());
-        System.out.println(solucionGreedy);
     }
 }
